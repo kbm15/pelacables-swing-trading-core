cb7e08a8e841d820bba011e56eb51bf62c6ef4fa
KBM15 <keyblademaster_01@hotmail.com>
2024-06-24 21:40:46 +0200

    Added cached tickers verification

--- TradingCore/tradingcore/utils/yahoo_finance.py
+++ TradingCore/tradingcore/utils/yahoo_finance.py
@@ -1,8 +1,20 @@
-import pandas as pd
+import os
+import pickle
 import yfinance as yf
+from datetime import datetime, timedelta
 
-def fetch_yahoo_finance_data(symbol: str, interval: str, period: str = None, start: str = None, end: str = None):
-    ticker = yf.Ticker(symbol)
+def load_cached_data(ticker):
+    cache_folder = 'cache'
+    cache_file = os.path.join(cache_folder, f'{ticker}_1h.pkl')
+    
+    if os.path.exists(cache_file):
+        with open(cache_file, 'rb') as f:
+            cached_data = pickle.load(f)
+        return cached_data
+    return None
+
+def fetch_yahoo_finance_data(ticker: str, interval: str, period: str = None, start: str = None, end: str = None):
+    ticker = yf.Ticker(ticker)
     
     if period:
         data = ticker.history(interval=interval, period=period)
@@ -21,15 +33,19 @@ def check_tickers_exist(tickers):
     invalid_tickers = []
     
     for ticker in tickers:
-        try:
-            # Fetch data for the ticker
-            data = yf.Ticker(ticker).history(period='1d')
-            # Check if data is returned
-            if not data.empty:
-                valid_tickers.append(ticker)
-            else:
-                invalid_tickers.append(ticker)
-        except Exception as e:
+        cached_data = load_cached_data(ticker)
+        
+        if cached_data is not None:
+            print(f"Using cached data for {ticker}")
+            data = cached_data
+        else:
+            #print(f"Fetching data for {ticker} from Yahoo Finance...")
+            # Example: Fetch data for the last 1 month with daily interval
+            data = fetch_yahoo_finance_data(ticker, interval='1d', period='1mo')
+        
+        if data is not None and not data.empty:
+            valid_tickers.append(ticker)
+        else:
             invalid_tickers.append(ticker)
     
     return valid_tickers, invalid_tickers
