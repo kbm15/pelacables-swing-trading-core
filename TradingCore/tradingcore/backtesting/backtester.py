b832486eec72caeb3d2e721e75e71a9a4c30b31a
KBM15 <keyblademaster_01@hotmail.com>
2024-06-24 21:42:02 +0200

    Removed slow df from backtesting

--- TradingCore/tradingcore/backtesting/backtester.py
+++ TradingCore/tradingcore/backtesting/backtester.py
@@ -10,56 +10,42 @@ class Backtester:
     def __init__(self, tsdata: TimeSeriesData, indicator: Indicator, initial_capital: float = 10000.0, purchase_fraction: float = 1.0, sell_fraction: float = 1.0):
         self.tsdata = tsdata
         self.indicator = indicator
+        self.initial_capital = initial_capital
         self.capital = initial_capital
         self.holdings = 0.0
         self.purchase_fraction = purchase_fraction
         self.sell_fraction = sell_fraction
-        self.data = pd.DataFrame(columns=['Portfolio Value', 'Holdings', 'Cash', 'Position'])
+        self.data = []
 
 
     def run_backtest(self):
-        
-        self.data['Position'] = self.indicator.calculate(self.tsdata.data)
-        self.data.loc[self.data.index[0],'Portfolio Value'] = 0.0
-        self.data.loc[self.data.index[0],'Holdings'] = self.holdings
-        self.data.loc[self.data.index[0],'Cash'] = self.capital
+        logging.debug(f'Starting indicator {self.indicator.strategy} on {self.tsdata.ticker}')
+        self.data = self.indicator.calculate(self.tsdata.data).tolist()
 
+        self.capital = self.initial_capital
+        self.holdings = 0.0
 
+        logging.debug(f'Running backtest {self.indicator.strategy} on {self.tsdata.ticker}')
         # Backtest logic here
-        total_holdings = self.data['Holdings'].iloc[0]
-        for i in range(1, len(self.tsdata.data)):
-            if  total_holdings < self.data['Holdings'].iloc[i-1]:
-                total_holdings = self.data['Holdings'].iloc[i-1]
-            if self.data['Position'].iloc[i-1] == 1:  # Comprar
-                amount_to_spend = min(self.data['Cash'].iloc[i-1], self.capital * self.purchase_fraction)
-                shares_bought = amount_to_spend / self.tsdata.data['Open'].iloc[i]
-                self.data.loc[self.data.index[i], 'Holdings'] = self.data['Holdings'].iloc[i-1] + shares_bought
-                self.data.loc[self.data.index[i], 'Cash'] = self.data['Cash'].iloc[i-1] - amount_to_spend
-                # self.data.loc[self.data.index[i], 'Take Profit'] = self.tsdata.data['Close'].iloc[i] * (1 + take_profit_pct)
-                # self.data.loc[self.data.index[i], 'Stop Loss'] = self.tsdata.data['Close'].iloc[i] * (1 - stop_loss_pct)
-            elif self.data['Position'].iloc[i] == -1:  # Vender
-                # if self.tsdata.data['Close'].iloc[i] >= data['Take Profit'][i-1]:
-                #     shares_to_sell = data['Holdings'].iloc[i-1] * self.sell_fraction
-                #     self.data.loc[self.data.index[i], 'Cash'] = self.data['Cash'].iloc[i-1] + shares_to_sell * self.tsdata.data['Close'].iloc[i]
-                #     self.data.loc[self.data.index[i], 'Holdings'] = self.data['Holdings'].iloc[i-1] - shares_to_sell
-                # elif self.tsdata.data['Close'].iloc[i] <= data['Stop Loss'][i-1]:
-                #     shares_to_sell = data['Holdings'].iloc[i-1] * self.sell_fraction
-                #     self.data.loc[self.data.index[i], 'Cash'] = self.data['Cash'].iloc[i-1] + shares_to_sell * self.tsdata.data['Close'].iloc[i]
-                #     self.data.loc[self.data.index[i], 'Holdings'] = self.data['Holdings'].iloc[i-1] - shares_to_sell
-                # else:
-                shares_to_sell = min(self.data['Holdings'].iloc[i-1], total_holdings * self.sell_fraction)
-                self.data.loc[self.data.index[i], 'Holdings'] = self.data['Holdings'].iloc[i-1] - shares_to_sell
-                self.data.loc[self.data.index[i], 'Cash'] = self.data['Cash'].iloc[i-1] + shares_to_sell * self.tsdata.data['Open'].iloc[i]
-            else:  # Mantener
-                self.data.loc[self.data.index[i], 'Holdings'] = self.data['Holdings'].iloc[i-1]
-                self.data.loc[self.data.index[i], 'Cash'] = self.data['Cash'].iloc[i-1]
-                # self.data.loc[self.data.index[i], 'Take Profit'] = self.data['Take Profit'][i-1]
-                # self.data.loc[self.data.index[i], 'Stop Loss'] = self.data['Stop Loss'][i-1]
+        max_holdings = self.holdings
+        for i in range(1, len(self.tsdata.data)):            
+            if (self.capital > 0.0) and (self.data[i] == 1):  # Comprar
+                amount_to_spend = min(self.capital, max(self.initial_capital,self.capital) * self.purchase_fraction)
+                shares_bought = amount_to_spend / self.tsdata.data['Close'].iloc[i]
+                self.holdings += shares_bought
+                self.capital -=  amount_to_spend
+                # Logica para vender fracciones
+                if  max_holdings < self.holdings:
+                    max_holdings = self.holdings
+            elif (self.holdings > 0.0) and (self.data[i] == -1):  # Vender
+                shares_to_sell = min(self.holdings, max_holdings * self.sell_fraction)
+                self.holdings -= shares_to_sell
+                self.capital +=  shares_to_sell * self.tsdata.data['Close'].iloc[i]
+                # Logica para vender fracciones
+                if  self.holdings == 0:
+                    max_holdings = self.holdings
 
-            self.data.loc[self.data.index[i], 'Portfolio Value'] = self.data['Cash'].iloc[i] + self.data['Holdings'].iloc[i] * self.tsdata.data['Close'].iloc[i]
-            self.data.loc[self.data.index[i], 'Shares Owned'] = self.data['Holdings'].iloc[i]
-
-        final_portfolio_value = self.data['Portfolio Value'].iloc[-1]
-        total_return = (final_portfolio_value - self.capital) / self.capital * 100
-        maximum_hold_return = self.tsdata.data['High'].max() /self.tsdata.data['Low'].min() * 100
-        return final_portfolio_value, total_return, maximum_hold_return
+        logging.debug(f'Finished backtest {self.indicator.strategy} on {self.tsdata.ticker}')
+        final_portfolio_value = self.capital + self.holdings * self.tsdata.data['Close'].iloc[i]
+        total_return = (final_portfolio_value - self.initial_capital) / self.initial_capital * 100
+        return final_portfolio_value, total_return
