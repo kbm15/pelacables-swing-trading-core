ec68fb9ecccd5a07e956408b45db4861d0dbd185
KBM15 <keyblademaster_01@hotmail.com>
2024-11-14 21:03:22 +0100

    Minor log on cron

--- orchestrator/orchestrator/amqp/handleSuscription.ts
+++ orchestrator/orchestrator/amqp/handleSuscription.ts
@@ -8,11 +8,9 @@ import { getSubscriptions,getUserSubscriptions,addUserSubscription,removeUserSub
 export async function scheduleSuscriptionQueries(channel: Channel,client: PostgresClient) {
     const openNYSE = DateTime.now()
                     .setZone("America/New_York")       // Set to ET
-                    .set({ hour: 9, minute: 30, second: 0, millisecond: 0 });
-    openNYSE.setZone('local')                
-    const cronString = `${openNYSE.minute} ${openNYSE.hour} * * *`;
+                    .set({ hour: 9, minute: 30, second: 0, millisecond: 0 });   
     //const cronString = `* * * * *`;
-    const job = new CronJob(cronString, async () => {  // Run daily at midnight
+    const job = new CronJob(openNYSE, async () => {  // Run daily at midnight
         console.log('Running subscription scheduler');
         const subscriptions = await getSubscriptions(client);
     
@@ -30,7 +28,7 @@ export async function scheduleSuscriptionQueries(channel: Channel,client: Postgr
 	true, // start
 	'America/Los_Angeles' // timeZone
     );
-    
+    console.log(`Scheduled subscription scheduler to run at ${job.nextDate().toISOTime()}`);
 }
 
 export function handleSuscriptions(channel: Channel,client: PostgresClient) {
