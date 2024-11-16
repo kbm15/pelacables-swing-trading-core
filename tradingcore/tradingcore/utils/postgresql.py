9cf32d1d583e75885cfd3a52917501976528035a
jarmi95 <jarmi_95@hotmail.com>
2024-11-16 09:32:44 +0100

    fix: changing db name to timeseries_db

--- tradingcore/tradingcore/utils/postgresql.py
+++ tradingcore/tradingcore/utils/postgresql.py
@@ -21,7 +21,7 @@ def connect_db():
             host=POSTGRES_HOST,
             user=POSTGRES_USER,
             password=POSTGRES_PASSWORD,
-            database=POSTGRES_DB
+            database='postgres'
         )
         return conn
     except Exception as error:
