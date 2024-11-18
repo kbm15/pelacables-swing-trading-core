eaa8dc13606bc0c1365f9dbadb95fe863d383161
jarmi95 <jarmi_95@hotmail.com>
2024-11-18 16:55:21 +0100

    fix: check signal content

--- telegram_app/telegram_app/amqp/handleNotification.ts
+++ telegram_app/telegram_app/amqp/handleNotification.ts
@@ -16,23 +16,27 @@ export async function consumeNotifications(channel: Channel, bot: Telegraf) {
     channel.consume(NOTIFICATION_QUEUE, (msg) => {
         if (msg) {
             const notification = JSON.parse(msg.content.toString());
-            console.log(`Notification received: ${notification}`);
+            console.log(`Notification received: ${JSON.stringify(notification)}`);
             const { chatId, ticker, signal } = notification;
 
             // Send buy/sell signal to the subscribed user
             const now = Date.now();
-            const recentSignals = Object.entries(signal).filter(([timestamp, value]) => {
-                const signalDate = new Date(Number(timestamp));
-                return now - signalDate.getTime() <= DAY_IN_MS && (value === 1 || value === -1);
-            });
+            const signalEntries = Object.entries(signal);
 
-            if (recentSignals.length > 0) {
-                const [timestamp, value] = recentSignals[0];
-                const signalString = value === 1 ? 'Compra' : 'Venta';
-                const signalDate = new Date(Number(timestamp));
-                const message = `📢 *${ticker}* señal de ${signalString} a las ${signalDate.toTimeString}!`;
-                bot.telegram.sendMessage(chatId, message, { parse_mode: 'Markdown' });
-                console.log(`Notification sent to userId: ${chatId} for ticker: ${ticker} with signal: ${signalString}`);
+            if (signalEntries != null && signalEntries != undefined) {
+                const recentSignals = signalEntries.filter(([timestamp, value]) => {
+                    const signalDate = new Date(Number(timestamp));
+                    return now - signalDate.getTime() <= DAY_IN_MS && (value === 1 || value === -1);
+                });
+        
+                if (recentSignals.length > 0) {
+                    const [timestamp, value] = recentSignals[0];
+                    const signalString = value === 1 ? 'Compra' : 'Venta';
+                    const signalDate = new Date(Number(timestamp));
+                    const message = `📢 *${ticker}* señal de ${signalString} a las ${signalDate.toTimeString()}!`;
+                    bot.telegram.sendMessage(chatId, message, { parse_mode: 'Markdown' });
+                    console.log(`Notification sent to userId: ${chatId} for ticker: ${ticker} with signal: ${signalString}`);
+                }
             }
             channel.ack(msg);
         }
